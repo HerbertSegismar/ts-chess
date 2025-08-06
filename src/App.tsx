a1aa5f2810872283d71ac36db8acdbb27d2bff71
Herb Segismar <herbert.segismar@yahoo.com>
2025-08-06 19:01:05 +1200

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+
+const App = () => {
+  return (
+    <div className="twflex w-screen min-h-screen">
+      <h1 className="text-3xl text-amber-400">Typescript Chess</h1>
+    </div>
+  )
+}
+
+export default App
